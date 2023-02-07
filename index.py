bfac7f95d5051cf3d56884ccc1fe0760bd0f4c74
Facu <saltyfacu@gmail.com>
2023-02-07 17:43:49 +0100

    chore: reinstante files

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from gpt_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-#from langchain import OpenAI
+from langchain import OpenAI
 
 app = Flask(__name__)
 CORS(app)
