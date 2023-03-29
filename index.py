24dd4a5980d091a94f2e459343367a9cbeaff6dd
Marco Guaspari Worms <marcogworms@gmail.com>
2023-03-29 13:36:27 -0700

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 max_input_size = 3700
 num_outputs = 300
-llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
 prompt_helper = PromptHelper.from_llm_predictor(llm_predictor)
 
 app = flask.Flask(__name__)
@@ -19,9 +19,9 @@ def ask():
   history = request.args.get('history')
   query = ''
   if history == 'none':
-    query = request.args.get('query') 
+    query = 'System\n' + 'You are yGenius, a Yearn Finance AI assistant.\n\n' + 'Anon:\n' request.args.get('query') 
   else:
-    query = history + '\n\n' + 'Anon:\n' + request.args.get('query') + '\n\nYou:\n'
+    query = history + '\n\n' + 'Anon:\n' + request.args.get('query') + '\n\nAI:\n'
   print(query)
   response = index.query(query, response_mode="default", prompt_helper=prompt_helper)
   print(response.response.strip())
